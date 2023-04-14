4a6d968a08419b191dafd44a732f6f0a5b87614f
RhianFelipe <rhian.felipe@hotmail.com>
2023-04-14 17:01:02 -0300

    Teste1

---
+++ php.php
@@ -0,0 +1,42 @@
+<?php
+/*
+* Change the value of $password if you have set a password on the root userid
+* Change NULL to port number to use DBMS other than the default using port 3306
+*
+*/
+$user = 'root';
+$password = ''; //To be completed if you have set a password to root
+$database = 'controlesistema'; //To be completed to connect to a database. The database must exist.
+$usertable = 'funcionario';
+$port = NULL; //Default must be NULL to use default port
+$mysqli = new mysqli('127.0.0.1', $user, $password, $database, $port);
+
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+}
+
+
+$sql = 'SELECT * FROM funcionario';
+$result = mysqli_query($mysqli,$sql);
+
+$func = mysqli_fetch_all($result,MYSQLI_ASSOC);
+
+print_r($func);
+
+
+$mysqli->close();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php ?>
+</body>
+</html>
