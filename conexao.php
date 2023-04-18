f2aee017532edc3ff0d359db8552c421e8a88f46
Rhian Felipe Silva de Lara <62002116+RhianFelipe@users.noreply.github.com>
2023-04-18 20:14:55 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -5,7 +5,7 @@ $database = 'controlesistema'; //To be completed to connect to a database. The d
 $usertable = 'funcionario';
 $port = NULL; //Default must be NULL to use default port
 $mysqli = new mysqli('127.0.0.1', $user, $password, $database, $port);
-
+//Verificar se ocorreu algum erro ao conectar com o Banco de Dados
 if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') '
             . $mysqli->connect_error);
