6e4803a18bdfa1c7257a0b36bb76e61ee691f56e
Rhian Felipe Silva de Lara <62002116+RhianFelipe@users.noreply.github.com>
2023-04-27 14:02:17 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $user = 'root';
-$password = ''; //To be completed if you have set a password to root
-$database = 'controlesistema'; //To be completed to connect to a database. The database must exist.
+$password = ''; 
+$database = 'controlesistema'; /
 $port = NULL; //Default must be NULL to use default port
 $mysqli = new mysqli('127.0.0.1', $user, $password, $database, $port);
 //Verificar se ocorreu algum erro ao conectar com o Banco de Dados
