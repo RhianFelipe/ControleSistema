35d6eb5cae25355378a671d8401b2b8ec51be862
Rhian Felipe Silva de Lara <62002116+RhianFelipe@users.noreply.github.com>
2023-05-08 17:03:09 -0300

    Add files via upload

---
+++ codTeste/adicionarBD.php
@@ -0,0 +1,122 @@
+<?php
+
+include "conexao.php";
+
+/*
+  $sql = 'SELECT * FROM usuarios';
+$result = mysqli_query($mysqli,$sql);
+
+while($row = mysqli_fetch_assoc($result)){
+echo $row["id"] ." - " .$row["nome"] . "<br>";
+
+
+}
+
+
+
+==================================
+<?php
+
+ include "conexao.php";
+
+
+
+if(isset($_POST) > 0){
+  $sql = 'SELECT * FROM usuarios';
+$result = mysqli_query($mysqli,$sql);
+
+while($row = mysqli_fetch_assoc($result)){
+echo $row["id"] ." - " .$row["nome"] . "<br>";
+
+
+}
+
+}
+$mysqli->close();
+
+?>
+==================================
+
+
+
+
+*/
+//$dsn = "mysql:host=$host;dbname=$database";
+//$pdo = new PDO($dsn, $user, $password);
+
+
+// Busca as permissões de um usuário específico
+function verificarExistencia($mysqli, $table, $whereClause) {
+  $sql = "SELECT COUNT(*) AS count FROM $table WHERE $whereClause";
+  $result = $mysqli->query($sql);
+  $count = $result->fetch_assoc()['count'];
+  return ($count > 0);
+}
+
+// Listar todos os usuários e suas permissões
+$sql = "SELECT * FROM usuarios,permissoes";
+$result = $mysqli->query($sql);
+
+echo "<h2>Usuários e suas permissões:</h2>";
+echo "<table><tr><th>ID</th><th>Nome</th><th>Sistemas</th><th>Permissão</th></tr>";
+while ($row = mysqli_fetch_assoc($result)) {
+  echo "<tr><td>".$row['id_usuario']."</td><td>".$row['nome']."</td><td>".$row['sistemas']."</td><td>".$row['permissao']."</td></tr>";
+}
+echo "</table>";
+
+// Adicionar um novo usuário e suas permissões
+if(count($_POST)>0){ 
+  
+  $nome = $_POST['nome'];
+  $sistema = $_POST['sistemas'];
+  $permissao = $_POST['permissao'];
+
+  $id_usuario = $mysqli->insert_id;
+  echo "ID:" . $id_usuario;
+ 
+$usuarioExiste = "SELECT nome FROM usuarios WHERE nome='$nome'";
+$resultUsuarioExiste = $mysqli->query($usuarioExiste) or die($mysqli->error);
+
+
+$sistemaExiste = "SELECT sistemas FROM permissoes WHERE sistemas='$sistema'";
+$resultSistemaExiste = $mysqli->query($sistemaExiste) or die($mysqli->error);
+
+
+if ($resultUsuarioExiste->num_rows >0) {
+  echo "Nome já existe no banco de dados!";
+  if($resultSistemaExiste->num_rows >  0){
+
+    echo "Sistema já existe no banco de dados!";
+    $sql = "UPDATE permissoes INNER JOIN usuarios ON permissoes.id_usuario = usuarios.id SET permissao='$permissao' WHERE nome='$nome' AND sistemas='$sistema'";
+    $deu = $mysqli->query($sql);
+   
+    if ($deu) {
+      echo "<p>Permissão atualizada com sucesso.</p>";
+      echo 'teste';
+      echo $id_usuario;
+      var_dump($_POST);
+    } else {
+      echo "<p>Ocorreu um erro ao atualizar a permissão.</p>";
+    }
+
+
+
+    
+  echo "Não existe o sistema";
+} else {
+  
+  echo "nao aq";
+
+   
+}
+}
+
+}
+
+
+mysqli_close($mysqli); // Fecho a conexão
+?>
+
+
+
+
---
+++ src/cadastrarUser.php
@@ -0,0 +1,35 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastar Usuário</title>
+</head>
+
+<body>
+    <form action="">
+        <a href="/">Voltar para Lista</a>
+        <form method="POST" action="">
+
+            <label>Nome:</label>
+            <input name="nome" type="text"><br>
+            <label>E-mail:</label>
+            <input name="email" type="text"><br>
+            <button type="submit">Cadastar</button>
+
+
+            
+
+        </form>
+
+        
+</body>
+
+</html>
