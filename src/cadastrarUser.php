c046d11a4f28bbd13c4d439fab3d47daf44d58fd
Rhian Felipe Silva de Lara <62002116+RhianFelipe@users.noreply.github.com>
2023-05-09 16:40:23 -0300

    Update cadastrarUser.php

--- src/cadastrarUser.php
+++ src/cadastrarUser.php
@@ -5,7 +5,6 @@
 
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,17 +15,11 @@
 </head>
 
 <body>
-
     <header>
         <a href="../index.php">Voltar para Filtro</a>
     </header>
 
-
-
     <div id="area-form">
-
-      
- 
         <form id="form" method="POST" action="">
         <h1>Cadastro Usuário</h1><br>
             <label>Nome:</label>
@@ -34,10 +27,7 @@
             <label>E-mail:</label>
             <input placeholder="usuario@pge.pr.gov.br" name="email" type="text" required><br>
             <button type="submit">Cadastrar</button>
-
         </form>
-
-
     </div>
 
 </body>
