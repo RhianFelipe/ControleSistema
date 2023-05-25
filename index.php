49bf819df2fec4c9a5a14cdebe848a24f822f837
GabrielTrizotti <gabrieltrizotti9@gmail.com>
2023-05-25 14:36:26 -0300

    Style: Estilização da tela filtro

--- index.php
+++ index.php
@@ -12,40 +12,43 @@ include "./db/conexao.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="./style/telaFiltro.css?v=<?php echo time(); ?>">
     <title>Sistema de Controle de Permissões</title>
 </head>
 
 <body>
     <header>
-        <h1>Controle de Sistemas</h1>
+        <img class="imgHeader" src="./assets\img/logo-govpr.png"></img>
         <a href="./src/cadastrarUser.php">Cadastrar Usuarios</a>
 
         <a href="">Lista de Usuários</a>
-
     </header>
 
+    <main>
+        <section>
+            <h1>Filtro de Controle de Cadastro</h1>
+            <div>
+                Nome: <input class="inputTable" type="text">
+                E-mail: <input class="inputTable" type="text">
+                Sistemas: 
+            
+                <select name="permissao">
+                    <option selected value="0">0</option>
+                    <option value="1">1</option>
+                </select>
 
-    <section>
-        <h1>Área de Consulta</h1>
-        <div>
-            <label> Nome:</label> <input class="inputTable" type="text">
-            <label> E-mail:</label> <input class="inputTable" type="text">
+                <label>Permissão:</label>
+                <select name="permissao">
+                    <option selected value="0">0</option>
+                    <option value="1">1</option>
+                </select>
+                teste
+            </div>
+        </section>
+    </main>
 
-            <label>Sistemas:</label>
-            <select name="permissao">
-                <option selected value="0">0</option>
-                <option value="1">1</option>
-            </select>
-
-            <label>Permissão:</label>
-            <select name="permissao">
-                <option selected value="0">0</option>
-                <option value="1">1</option>
-            </select>
-            teste
-        </div>
-    </section>
-
+    <footer>
+        Todos os direitos reservados 
+    </footer>
 </body>
-
 </html>
