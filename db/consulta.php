c8b19edb7695fcdb2de8c2dca5ee914c7b2b9979
Rhian Felipe Silva de Lara <62002116+RhianFelipe@users.noreply.github.com>
2023-05-29 20:51:35 -0300

    Update consulta.php

--- db/consulta.php
+++ db/consulta.php
@@ -17,8 +17,6 @@ $queryBuscaSistemas =  $mysqli->query($buscaSistemas) or die($mysqli->error);
 // Consulta os grupos de cada funcionario(procuradores, estags, servidores...)
 $buscaGrupo = "SELECT grupo FROM usuarios"; 
 $queryBuscaGrupo = $mysqli->query($buscaGrupo) or die($mysqli->error);
-
-
  ?>
 
 
