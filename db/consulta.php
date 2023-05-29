776e122511d286774c051d7e0364b0c4bb0b56ff
Rhian Felipe Silva de Lara <62002116+RhianFelipe@users.noreply.github.com>
2023-05-29 20:50:48 -0300

    Update consulta.php

--- db/consulta.php
+++ db/consulta.php
@@ -1,6 +1,17 @@
 <?php 
  include "../db/conexao.php";
 
+
+//Função que faz consultar para verificar se existe algum atributo no Banco de Dados
+function verificarExistencia($mysqli, $valor, $tabela, $variavel)
+{
+    $verificar = "SELECT $valor FROM $tabela WHERE $valor='$variavel'";
+    $resultVerificacao = $mysqli->query($verificar) or die($mysqli->error);
+    return $resultVerificacao;
+}
+
+
+
 // Consulta os sistemas e as permissões disponíveis no banco de dados
 $buscaSistemas = "SELECT sistemas FROM permissoes";
 $queryBuscaSistemas =  $mysqli->query($buscaSistemas) or die($mysqli->error);
