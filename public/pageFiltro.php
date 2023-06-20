7217d83f1aacf8680ed3198c311e624db520132a
RhianFelipe <rhian.felipe@hotmail.com>
2023-06-20 00:43:37 -0300

    Refatoração de código

--- public/pageFiltro.php
+++ public/pageFiltro.php
@@ -11,7 +11,6 @@ include "../src/popup.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../public/main.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="../public/style/telaFiltro.css?v=<?php echo time(); ?>">
-    <link rel="stylesheet" href="../public/style/popup.css?v=<?php echo time(); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
@@ -112,7 +111,6 @@ include "../src/popup.php";
                         <form class="row g-3" id="edit-usuario-form">
                             <input type="hidden" name="id" id="editid">
                             <div class="col-12">
-
                                 <div class="col-12">
 
                                     <table>
@@ -123,8 +121,6 @@ include "../src/popup.php";
                                         <tr>
                                             <td id="sistemasEdit"></td>
                                             <td id="permissaoEdit"></td>
-
-
                                         </tr>
                                     </table>
                                 </div>
@@ -146,129 +142,7 @@ include "../src/popup.php";
         Todos os direitos reservados
     </footer>
 
-    <script>
-    async function openPopup(id) {
-        console.log(id)
-
-        const dados = await fetch('../src/editarUser.php?id=' + id);
-        const resposta = await dados.json();
-
-        console.log(resposta);
-        if (!resposta['status']) {
-            document.getElementById("msgAlerta").innerHTML = resposta['msg']
-        } else {
-            const editModel = new bootstrap.Modal(document.getElementById("editUsuarioModal"))
-            editModel.show()
-            const idUsuario = resposta['dados'][0].id_usuario;
-            document.getElementById("editid").value = idUsuario;
-            console.log("ID:", idUsuario);
-            const sistemasEdit = document.getElementById('sistemasEdit');
-            sistemasEdit.innerHTML = ""; // Limpar o conteúdo existente, se houver
-            console.log("Dados:", resposta['dados'])
-
-
-            const sistemas = resposta['dados'].map(obj => obj.sistemas);
-            console.log("Sistemas Mapeados: ",sistemas);
-            sistemas.forEach(sistema => {
-                const li = document.createElement('li');
-                li.textContent = sistema;
-                sistemasEdit.appendChild(li);
-
-                // Criar input oculto para o sistema
-                const inputSistema = document.createElement('input');
-                inputSistema.type = 'hidden';
-                inputSistema.name = 'sistema[]';
-                inputSistema.value = sistema;
-                editForm.appendChild(inputSistema);
-            });
-
-            const permissaoEdit = document.getElementById('permissaoEdit');
-            permissaoEdit.innerHTML = ""; // Limpar o conteúdo existente, se houver
-
-            const permissoes = resposta['dados'].map(obj => obj.permissao);
-            console.log("Permissões Mapeadas: ", permissoes);
-
-            permissoes.forEach(permissao => {
-                const checkbox = document.createElement('input');
-                checkbox.type = 'checkbox';
-                checkbox.checked = permissao === '1'; // Marca o checkbox se a permissao for igual a '1'
-
-                const td = document.createElement('td');
-                td.style.padding = '4.3'; // Ajustar o espaçamento interno da célula
-
-                const label = document.createElement('label');
-                label.style.margin = '0'; // Ajustar o espaçamento externo do rótulo
-                label.appendChild(checkbox);
-
-                td.appendChild(label);
-
-                const tr = document.createElement('tr');
-                tr.appendChild(td);
-
-                permissaoEdit.appendChild(tr);
-
-                // Criar input oculto para a permissao
-                const inputPermissao = document.createElement('input');
-                inputPermissao.type = 'hidden';
-                inputPermissao.name = 'permissao[]';
-                inputPermissao.value = permissao;
-                editForm.appendChild(inputPermissao);
-            });
-        }
-    }
-
-
-
-
-
-
-    const editForm = document.getElementById("edit-usuario-form");
-    if (editForm) {
-        editForm.addEventListener("submit", async (e) => {
-            e.preventDefault();
-                    console.log("Entrou aqui");
-            const idUsuario = document.getElementById("editid").value;
-            const sistemas = [];
-            const permissoes = [];
-            const checkboxes = editForm.querySelectorAll('input[type="checkbox"]');
-
-            checkboxes.forEach(checkbox => {
-             
-                const permissao = checkbox.checked ? '1' : '0';
-
-             
-                permissoes.push(permissao);
-            });
-            const sistemasElements = editForm.querySelectorAll('[name="sistema[]"]');
-sistemasElements.forEach(sistemaElement => {
-  const sistema = sistemaElement.value;
-  sistemas.push(sistema);
-});
-            
-alert('Foi Salvo');
-            const dadosForm = new FormData();
-            dadosForm.append('id', idUsuario);
-            sistemas.forEach(sistema => {
-                dadosForm.append('sistema[]', sistema);
-            });
-            permissoes.forEach(permissao => {
-                dadosForm.append('permissao[]', permissao);
-            });
-
-            console.log("ID:", idUsuario);
-            console.log("Sistemas:", sistemas);
-            console.log("Permissões:", permissoes);
-            const dados = await fetch("../src/updateUser.php", {
-                method: "POST",
-                body: dadosForm
-            });
-            const resposta = await dados.json();
-            console.log("Resposta do edit form:", resposta);
-        });
-        alert('Foi Salvo');
-    }
-
-    </script>
+    <script src="../script/popupEdit.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
---
+++ script/popupEdit.js
@@ -0,0 +1,113 @@
+ async function openPopup(id) {
+        console.log(id)
+
+        const dados = await fetch('../src/viewUser.php?id=' + id);
+        const resposta = await dados.json();
+
+        console.log(resposta);
+        if (!resposta['status']) {
+            document.getElementById("msgAlerta").innerHTML = resposta['msg']
+        } else {
+            const editModel = new bootstrap.Modal(document.getElementById("editUsuarioModal"))
+            editModel.show()
+            const idUsuario = resposta['dados'][0].id_usuario;
+            document.getElementById("editid").value = idUsuario;
+            console.log("ID:", idUsuario);
+            const sistemasEdit = document.getElementById('sistemasEdit');
+            sistemasEdit.innerHTML = ""; // Limpar o conteúdo existente, se houver
+            console.log("Dados:", resposta['dados'])
+
+
+            const sistemas = resposta['dados'].map(obj => obj.sistemas);
+            console.log("Sistemas Mapeados: ", sistemas);
+            sistemas.forEach(sistema => {
+                const li = document.createElement('li');
+                li.textContent = sistema;
+                sistemasEdit.appendChild(li);
+
+                // Criar input oculto para o sistema
+                const inputSistema = document.createElement('input');
+                inputSistema.type = 'hidden';
+                inputSistema.name = 'sistema[]';
+                inputSistema.value = sistema;
+                editForm.appendChild(inputSistema);
+            });
+
+            const permissaoEdit = document.getElementById('permissaoEdit');
+            permissaoEdit.innerHTML = ""; // Limpar o conteúdo existente, se houver
+
+            const permissoes = resposta['dados'].map(obj => obj.permissao);
+            console.log("Permissões Mapeadas: ", permissoes);
+
+            permissoes.forEach(permissao => {
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.checked = permissao === '1'; // Marca o checkbox se a permissao for igual a '1'
+
+                const td = document.createElement('td');
+                td.style.padding = '4.3'; // Ajustar o espaçamento interno da célula
+
+                const label = document.createElement('label');
+                label.style.margin = '0'; // Ajustar o espaçamento externo do rótulo
+                label.appendChild(checkbox);
+
+                td.appendChild(label);
+
+                const tr = document.createElement('tr');
+                tr.appendChild(td);
+
+                permissaoEdit.appendChild(tr);
+
+                // Criar input oculto para a permissao
+                const inputPermissao = document.createElement('input');
+                inputPermissao.type = 'hidden';
+                inputPermissao.name = 'permissao[]';
+                inputPermissao.value = permissao;
+                editForm.appendChild(inputPermissao);
+            });
+        }
+    }
+
+    const editForm = document.getElementById("edit-usuario-form");
+    if (editForm) {
+        editForm.addEventListener("submit", async (e) => {
+            e.preventDefault();
+            console.log("Entrou aqui");
+            const idUsuario = document.getElementById("editid").value;
+            const sistemas = [];
+            const permissoes = [];
+            const checkboxes = editForm.querySelectorAll('input[type="checkbox"]');
+
+            checkboxes.forEach(checkbox => {
+                const permissao = checkbox.checked ? '1' : '0';
+                permissoes.push(permissao);
+            });
+            const sistemasElements = editForm.querySelectorAll('[name="sistema[]"]');
+            sistemasElements.forEach(sistemaElement => {
+                const sistema = sistemaElement.value;
+                sistemas.push(sistema);
+            });
+
+            const dadosForm = new FormData();
+            dadosForm.append('id', idUsuario);
+            sistemas.forEach(sistema => {
+                dadosForm.append('sistema[]', sistema);
+            });
+            permissoes.forEach(permissao => {
+                dadosForm.append('permissao[]', permissao);
+            });
+
+            console.log("ID:", idUsuario);
+            console.log("Sistemas:", sistemas);
+            console.log("Permissões:", permissoes);
+            const dados = await fetch("../src/updateUser.php", {
+                method: "POST",
+                body: dadosForm
+            });
+            const resposta = await dados.json();
+            console.log("Resposta do edit form:", resposta);
+            alert('Foi Salvo');
+        });
+
+    }
+  
