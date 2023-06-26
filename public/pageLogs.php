4c020abcc24e77b61dadd6427566d131610eb860
RhianFelipe <rhian.felipe@hotmail.com>
2023-06-26 16:56:38 -0300

    Estilização dos Logs

--- public/pageLogs.php
+++ public/pageLogs.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>Histórico de Logs de Usuários</title>
     <link rel="stylesheet" href="../public/main.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../public/style/telaLogs.css?v=<?php echo time(); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
@@ -21,10 +22,11 @@
           
         </nav>
     </header>
+
     <section>
 
 
-        <h1>Histórico de Logs de Usuários</h1>
+    <h1>Histórico de Logs de Usuários</h1>
 
         <?php
     include "../db/conexao.php";
