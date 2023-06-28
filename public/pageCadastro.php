6adc6c43e7991159135df6766be753583b6d1a13
RhianFelipe <rhian.felipe@hotmail.com>
2023-06-28 15:04:23 -0300

    Update: Gerenciar Permissões agora usa valores do banco de dados

--- public/pageCadastro.php
+++ public/pageCadastro.php
@@ -1,5 +1,3 @@
-
-
 <?php 
 // Incluir arquivo de conexão com o banco de dados e arquivo de pop-up
 include "../src/cadastrarUser.php";
@@ -17,6 +15,7 @@ include "../src/cadastrarUser.php";
     <link rel="stylesheet" href="../public/style/telaCadastro.css?v=<?php echo time(); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <script src="../script/utils.js"></script>
     <title>Cadastrar Usuário</title>
 </head>
 
@@ -25,7 +24,7 @@ include "../src/cadastrarUser.php";
     <header>
         <img class="imgHeader" src="..\public\assets\img\logo-govpr-white.png">
         <nav class="navbar">
-            
+
             <li class="list-header"><a class="a1" href="../public/pageFiltro">Voltar para Filtro</a> </li>
         </nav>
     </header>
@@ -35,12 +34,11 @@ include "../src/cadastrarUser.php";
         <form id="form" method="POST" action="../public/pageCadastro.php">
             <h1>Cadastrar Usuário</h1><br>
             <label>Nome:</label>
-            <input class="input-value" id="nome" value="<?php if(isset($_POST['nome']))?>"
-                placeholder="nome" name="nome" type="text" required><br>
+            <input class="input-value" id="nome" value="" placeholder="nome" name="nome" type="text" required><br>
 
             <label>E-mail:</label>
-            <input class="input-value" value="<?php if(isset($_POST['email'])) ?>"
-                placeholder="usuario@pge.pr.gov.br" name="email" type="text" required><br>
+            <input class="input-value" value="" placeholder="usuario@pge.pr.gov.br" name="email" type="text"
+                required><br>
 
             <label>Grupo:</label>
             <!-- Obter valores dos grupos do banco de dados e mostrá-los em um menu suspenso -->
@@ -48,7 +46,7 @@ include "../src/cadastrarUser.php";
                 <?php
                 $gruposPermitidos = array("Procurador", "Servidor", "Tercerizado", "Estagiário", "Advogado");
                 foreach ($gruposPermitidos as $grupoPermitido) {
-                    echo "<option value='$grupoPermitido'>$grupoPermitido</option>";
+                echo "<option value='$grupoPermitido'>$grupoPermitido</option>";
                 }
                 ?>
             </select> <br>
@@ -57,38 +55,54 @@ include "../src/cadastrarUser.php";
 
             <button onclick="gerenciarPermissoes() " id="button-permissao" type="button">Permissões</button> <br>
             <div id="selects-permissoes" style="display: none; ">
-                <h3>Permissões:</h3>
-                <p>Marque as permissões para cada sistema:</p>
+           
                 <?php
-                $sistemas = array(
-                    "admIntranet" => "ADM/INTRANET",
-                    "admInternet" => "ADM/INTERNET",
-                    "arisp" => "ARISP",
-                    "copel" => "Copel",
-                    "detran" => "Detran",
-                    "documentador" => "Documentador",
-                    "sipro" => "Sipro",
-                    "eProtocolo" => "eProtocolo"
-                );
-                foreach ($sistemas as $nomeSistema => $descricaoSistema) {
-                    echo "<div class='selects-permissoes'>";
-                    echo "<label for='$nomeSistema'>$descricaoSistema:</label>";
-                    echo "<select name='sistemas[$nomeSistema]' id='$nomeSistema'>"; // Adiciona o atributo id
-                    echo "<option value='0'>0</option>";
-                    echo "<option value='1'>1</option>";
-                    echo "</select>";
-                    echo "</div><br><br>";
-                }
-                ?>
+          include "../db/conexao.php";
+
+          $sql = "SELECT DISTINCT nomeSistema FROM permissoes";
+          $result = mysqli_query($mysqli, $sql);
+          
+          // Verificar se a consulta teve resultados
+          if (mysqli_num_rows($result) > 0) {
+              $sistemas = array();
+          
+              // Loop pelos resultados da consulta
+              while ($row = mysqli_fetch_assoc($result)) {
+                  $nomeSistema = $row['nomeSistema'];
+          
+                  // Verificar se o nome do sistema é válido
+                  if (!empty($nomeSistema) && strpos($nomeSistema, ':') === false) {
+                      $sistemas[] = $nomeSistema;
+                  }
+              }
+              // Mostrar os valores em um select
+              if (!empty($sistemas)) {
+                  echo "<div id='selects-permissoes'>";
+                  foreach ($sistemas as $nomeSistema) {
+                      echo "<div class='selects-permissoes'>";
+                      echo "<label for='$nomeSistema'>$nomeSistema:</label>";
+                      echo "<select name='sistemas[$nomeSistema]' id='$nomeSistema'>";
+                      echo "<option value='0'>Não</option>";
+                      echo "<option value='1'>Sim</option>";
+                      echo "</select>";
+                      echo "</div><br><br>";
+                  }
+                  echo "</div>";
+              }
+          }
+          
+          // Fechar a conexão com o banco de dados
+          mysqli_close($mysqli);
+          ?>
             </div>
             <button id="button-submit" type="submit">Cadastrar</button>
         </form>
     </div>
 
     <footer>
-     <p>&copy; 2023 Procuradoria Geral do Estado do Paraná. Todos os direitos reservados.</p>
+        <p>&copy; 2023 Procuradoria Geral do Estado do Paraná. Todos os direitos reservados.</p>
     </footer>
-    <script src="../script/utils.js"></script>
+   
 </body>
 
 </html>
