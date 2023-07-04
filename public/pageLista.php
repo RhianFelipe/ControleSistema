184c49368d8697f451e797337de8c597fef549ee
RhianFelipe <rhian.felipe@hotmail.com>
2023-07-04 17:16:51 -0300

    Style: Estilização do main e tela Filtro

--- public/pageLista.php
+++ public/pageLista.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION['user'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../public/main.css?v=<?php echo time(); ?>">
-    <link rel="stylesheet" href="../public/style/telaFiltro.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../public/style/telaLista.css?v=<?php echo time(); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <link rel="icon" href="../public/assets/img/icon-govpr.png" type="image/x-icon">
--- src/sistema/modalSistema.php
+++ src/sistema/modalSistema.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="editUsuarioModalLabel">Editar Usuário</h5>
+                <h5 class="modal-title" id="editUsuarioModalLabel">Editar Sistema</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
